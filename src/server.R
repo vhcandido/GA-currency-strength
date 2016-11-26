79762b94ff661ce224d34c407256a2093ca0f2fd
Victor Hugo Candido de Oliveira <victorhcandido@gmail.com>
2016-11-26 00:20:38 -0200

    Separating the server from backtesting of the chromosome

---
+++ src/server.R
@@ -0,0 +1,19 @@
+source('strategy.R')
+
+# Chromosome received from GA
+chromo <- paste("1,6",
+								paste(rep(15,21), collapse = ','),
+								paste(rep(72,21), collapse = ','),
+								paste(rep(6,21), collapse = ','),
+								paste(rep(4,7), collapse = ','),
+								paste(rep(8,21), collapse = ','),
+								paste(rep(5,21), collapse = ','),
+								paste(rep(14,21), collapse = ','),
+								paste(rep(5,21), collapse = ','),
+								paste(rep(5,21), collapse = ','),
+								paste(rep(3,21), collapse = ','),
+								paste(rep(15,21), collapse = ','),
+								paste(rep(3,21), collapse = ','),
+								sep=',')
+
+final.balance <- chromo.backtest(chromo)
