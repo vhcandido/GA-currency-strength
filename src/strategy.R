1124afc76957cfb9b291301367e2fd903c300cfd
Victor Hugo Candido de Oliveira <victorhcandido@gmail.com>
2016-11-27 16:05:39 -0200

    Treated (catching) errors and added upper and lower limits for some parameters

--- src/strategy.R
+++ src/strategy.R
@@ -4,8 +4,15 @@ source('BackTest.R')
 cat('Ready to run\n\n')
 #source('Plot.R')
 
-parse.genes <- function(par, genes, names, n) {
-	params <- as.list(genes[c(1:n)])
+name.genes <- function(par, genes, names, n, max = 0, allow0 = T) {
+	g <- genes[c(1:n)]
+	if(max != 0) {
+		g <- g %% max
+		if(!allow0) {
+			g[which(g==0)] <- max
+		}
+	}
+	params <- as.list(g)
 	names(params) <- names
 	return(append(par, list(params)))
 }
@@ -18,44 +25,51 @@ chromo.backtest <- function(chromo, debug=FALSE) {
 	genes <- as.numeric(unlist(strsplit(chromo, ',')))
 	
 	# Taking the first 2 -> orderRisk and maxTotalRisk
-	risk <- genes[c(1,2)]/100
+	# \in [1,10]
+	risk <- (genes[c(1,2)] %% 10) / 100
+	risk[which(risk==0)] <- 10
 	genes <- genes[-c(1,2)] # remove from genes
 	
 	# Taking the next 21 -> UOO.1.pips_until_SL
-	par <- parse.genes(par, genes, ev$pairsTotal, 21)
+	par <- name.genes(par, genes, ev$pairsTotal, 21)
 	genes <- genes[-c(1:21)] # remove from genes
 	
-	# Taking the next 21 -> SP.2StrMat for:
-	# n_sma, min_diff
-	for(i in 1:2) {
-		par <- parse.genes(par, genes, ev$pairsTotal, 21)
-		genes <- genes[-c(1:21)] # remove from genes
-	}
+	# Taking the next 21 -> SP.2StrMat.n_sma
+	par <- name.genes(par, genes, ev$pairsTotal, 21)
+	genes <- genes[-c(1:21)] # remove from genes
+	
+	# Taking the next 21 -> SP.2StrMat.min_diff
+	# \in [0, 24)
+	par <- name.genes(par, genes, ev$pairsTotal, 21, max=24, allow0=T)
+	genes <- genes[-c(1:21)] # remove from genes
 	
 	# Taking the next 7 -> SP.2StrMat.min_strength
-	par <- parse.genes(par, genes, ev$currencies, 7)
+	# \in [0, 12)
+	par <- name.genes(par, genes, ev$currencies, 7, max=12, allow0=T)
 	genes <- genes[-c(1:7)] # remove from genes
 	
 	# Taking the next 21 -> F.Sma for:
 	# n_sma, n_pips
 	for(i in 1:2) {
-		par <- parse.genes(par, genes, ev$pairsTotal, 21)
+		par <- name.genes(par, genes, ev$pairsTotal, 21)
 		genes <- genes[-c(1:21)] # remove from genes
 	}
 	
 	# Taking the next 21 -> F.StochRsi for:
 	# nrsi, nFastK, nFastD, nSlowD
 	for(i in 1:4) {
-		par <- parse.genes(par, genes, ev$pairsTotal, 21)
+		par <- name.genes(par, genes, ev$pairsTotal, 21)
 		genes <- genes[-c(1:21)] # remove from genes
 	}
 	
-	# Taking the next 21 -> RM.1 for:
-	# n_pips_to_Uturn, tp_by_sl
-	for(i in 1:2) {
-		par <- parse.genes(par, genes, ev$pairsTotal, 21)
-		genes <- genes[-c(1:21)] # remove from genes
-	}
+	# Taking the next 21 -> RM.1.n_pips_to_Uturn
+	par <- name.genes(par, genes, ev$pairsTotal, 21)
+	genes <- genes[-c(1:21)] # remove from genes
+	
+	# Taking the next 21 -> RM.1.tp_by_sl
+	# \in [1,5]
+	par <- name.genes(par, genes, ev$pairsTotal, 21, max=5, allow0=F)
+	genes <- genes[-c(1:21)] # remove from genes
 	
 	par.names = c(
 		'UOO.1.pips_until_SL',# = 15,
@@ -74,20 +88,27 @@ chromo.backtest <- function(chromo, debug=FALSE) {
 	# Append the remaining genes and name the list
 	par <- append(par, as.list(genes))
 	names(par) <- par.names
-
-	#if(debug) { print(par) }
 	
-	out <- backtest(strategy=S.2,
+	if(debug) { print(par) }
+	out <- tryCatch(
+				backtest(strategy=S.2,
 									par = par,
 									dataInt = '2015-10-01::2015-10-10',
 									#spread = 3,
-									accBalance = 250,
+									accBalance = 250.,
 									orderRisk = risk[1],
 									maxTotalRisk = risk[2],
 									#logFile = 'Log.log',
-									enable.output = F)
+									enable.output = F),
+				warning = function(w) { cat('Warning: '); print(w); return(0.); },
+				error = function(e) { cat('Error: '); print(e); return(0.); })
 	
-	final.balance <- as.numeric(tail(out$balanceTS,1))
+	if(is.list(out)) {
+		final.balance = as.numeric(tail(out$balanceTS,1))
+	} else {
+		final.balance = out
+	}
+	
 	return(final.balance)
 }
 
