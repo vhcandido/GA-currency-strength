0df3192109246bbaf76df26cc4fb57f58df8fa67
Victor Hugo Candido de Oliveira <victorhcandido@gmail.com>
2016-11-27 16:04:02 -0200

    Improving debugging and visualization

--- src/server.R
+++ src/server.R
@@ -34,20 +34,20 @@ con <- socketConnection(
 			server = TRUE,
 			open = 'r+')
 while(TRUE) {
-	cat('\nListening...\n')
+	cat('\n\nListening...\n')
 	input <- readLines(con, 1)
 
 	if(length(input) == 0) { cat('No message received. Exiting...\n'); break; }
-	if(input != 'NEWGEN') { break; }
+	if(input != 'NEWGEN') { cat('No NEWGEN received\n'); break; }
 
 	while(TRUE) {
-		cat('Waiting for the next chromosome\n')
+		cat('\nWaiting for the next chromosome\n')
 		input <- readLines(con, 1)
-		if(input == 'ENDGEN') { break; }
+		if(input == 'ENDGEN') { cat('ENDGEN received\n'); break; }
 
 		# Evaluate fitness
 		cat('Calling backtest\n')
-		final.balance <- chromo.backtest(input, T)
+		final.balance <- chromo.backtest(input, F)
 		cat('Fitness: ', final.balance, '\n\n')
 
 		# Send the fitness to the client
