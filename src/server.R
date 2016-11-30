0bf2a817ac00b93c19ab64b7c14ba05b5434cb6a
Victor Hugo Candido de Oliveira <victorhcandido@gmail.com>
2016-11-29 22:01:44 -0200

    Removed default chromosome from GA server (it's not needed here)

--- src/server.R
+++ src/server.R
@@ -1,21 +1,5 @@
 source('strategy.R')
 
-# Example of chromosome received from GA
-chromo <- paste("1,6",
-		paste(rep(15,21), collapse = ','),
-		paste(rep(72,21), collapse = ','),
-		paste(rep(6,21), collapse = ','),
-		paste(rep(4,7), collapse = ','),
-		paste(rep(8,21), collapse = ','),
-		paste(rep(5,21), collapse = ','),
-		paste(rep(14,21), collapse = ','),
-		paste(rep(5,21), collapse = ','),
-		paste(rep(5,21), collapse = ','),
-		paste(rep(3,21), collapse = ','),
-		paste(rep(15,21), collapse = ','),
-		paste(rep(3,21), collapse = ','),
-		sep=',')
-
 # Read parameters from stdin
 param <- commandArgs(trailingOnly = TRUE)
 if(length(param) < 1) {
